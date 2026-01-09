499da657f7bf4954ac13cee216bcc2bdb287362e
clarksprojectsdev <clarksprojectsdev@users.noreply.github.com>
2026-01-09 12:11:42 -0500

    Move expo-router to optionalDependencies and block it in Metro config for web builds

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Exclude expo-router from web builds to prevent htmlRoutes errors
+if (process.env.EXPO_PUBLIC_PLATFORM === 'web' || process.env.PLATFORM === 'web') {
+  config.resolver = {
+    ...config.resolver,
+    blockList: [
+      ...(config.resolver?.blockList || []),
+      /node_modules\/expo-router/,
+    ],
+  };
+}
+
 module.exports = config;
