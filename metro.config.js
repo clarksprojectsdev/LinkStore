377ed0afdd5e59f231375dbd4bde49309d77aeee
clarksprojectsdev <clarksprojectsdev@users.noreply.github.com>
2026-01-09 10:17:57 -0500

    Add clear flag and configure Metro for web platform

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure web platform is properly configured
+config.resolver.platforms = ['web', 'native', 'ios', 'android'];
+
 module.exports = config;
