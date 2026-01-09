0e02806084d3336a37af22f33e201f75cbfa833b
clarksprojectsdev <clarksprojectsdev@users.noreply.github.com>
2026-01-09 10:19:01 -0500

    Simplify build command and metro config

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure web platform is properly configured
-config.resolver.platforms = ['web', 'native', 'ios', 'android'];
-
 module.exports = config;
